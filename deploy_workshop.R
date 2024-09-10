4b42521aecb19d31421bb81ae97e00ebc1311f00
Yue Cao <yue.cao.au@gmail.com>
2024-09-10 18:05:25 +0800

    Update project id

--- deploy_workshop.R
+++ deploy_workshop.R
@@ -1,62 +1,29 @@
 
-
-
-Sys.setenv(GCE_AUTH_FILE = 'C:/Users/xifu0689/OneDrive - The University of Sydney (Staff)/Conferences/ISMB_Cornell_Glasgow_2024/Workshop/scdney-2024-c8c3291c1d7b.json')
+Sys.setenv(GCE_AUTH_FILE = '/Users/yue/Downloads/semiotic-vial-255604-401233702f0c.json')
 library(googleComputeEngineR)
 
 
-gce_global_project("scdney-2024")
+gce_global_project("semiotic-vial-255604")
 gce_global_zone("us-central1-a")
 
 ## see gce_list_machinetype() for options of predefined_type
-(tag = "gcr.io/scdney-2024/workshop:alpha")
+(tag = "gcr.io/semiotic-vial-255604/diseaseprediction:alpha")
 
-
+the_list <- gce_list_instances()
 
 vm1 <- gce_vm(template = "rstudio",
-              name = "workshop1",
+              name = "diseaseprediction",
               disk_size_gb = 500,
               predefined_type = "n2-standard-32",
               dynamic_image = tag,
               user = "rstudio",
-              password = "2024")
-
-
-gce_global_zone("asia-southeast1-a")
-
-
-vm2 <- gce_vm(template = "rstudio",
-              name = "workshop2",
-              disk_size_gb = 500,
-              predefined_type = "n2-standard-32",
-              dynamic_image = tag,
-              user = "rstudio",
-              password = "2024")
-
-
+              password = "test")
 
 # gce_global_zone("us-west2-a")
-
-# vm2 <- gce_vm(template = "rstudio",
-#              name = "workshop2",
-#              disk_size_gb = 500,
-#              predefined_type = "n2-standard-32",
-#              dynamic_image = tag,
-#              user = "rstudio",
-#              password = "test")
-
-# 32 CPU	256GB RAM
-# system(paste0("cp -r /home/gittmp/* ", getwd()))
-
-
-# 
-# 
-# 
-# vm1 = gce_ssh_setup(vm1,
-#                     username = "rstudio",
-#                     key.pub = "~/.ssh/id_rsa.pub",
-#                     key.private = "~/.ssh/id_rsa")
-# 
-# gce_push_registry(vm1,
-#                   save_name = "customised",
-#                   container_name = "rstudio")
+vm2 <- gce_vm(template = "rstudio",
+             name = "diseaseprediction2",
+             disk_size_gb = 500,
+             predefined_type = "n2-standard-32",
+             dynamic_image = tag,
+             user = "rstudio",
+             password = "test")
